bfdb9b971402eff75dc2a8814c17fbcc19bd0930
Johannes Siipola <johannes@siipo.la>
2021-10-02 15:22:09 +0300

    Initial commit

---
+++ cache-refresher.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * Plugin Name: Cache Refresher
+ * Description: A plugin to refresh your cache periodically or on demand
+ * Version: 0.0.1
+ */
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/woocommerce/action-scheduler/action-scheduler.php';
+
+function cache_refresher_run()
+{
+    $public_post_types = get_post_types(['public' => true]);
+    $public_post_types = array_keys($public_post_types);
+
+    $query = new WP_Query([
+        'post_type' => $public_post_types,
+        'posts_per_page' => -1,
+        'fields' => 'ids',
+    ]);
+
+    $posts = $query->posts;
+
+    $count = count($posts);
+
+    cache_refresher_wp_cli_echo("Found $count posts to refresh.");
+
+    foreach ($posts as $post) {
+        $url = get_permalink($post);
+        $next_action = as_next_scheduled_action('cache_refresher_process_post', [$post, $url]);
+        if ($next_action === false) {
+            as_enqueue_async_action('cache_refresher_process_post', [$post, $url]);
+            cache_refresher_wp_cli_echo("Added $post ($url) to refresh queue");
+        } else {
+            cache_refresher_wp_cli_echo("Post $post ($url) is already queued.");
+        }
+    }
+    cache_refresher_wp_cli_echo("All $count posts have been successfully processed");
+}
+
+function cache_refresher_wp_cli_echo($line) {
+    if (class_exists('WP_CLI')) {
+        WP_CLI::line($line);
+    }
+}
+
+function cache_refresher_init()
+{
+    $cron_name = apply_filters('cache_refresher_cron_name', 'daily');
+    add_action('cache_refresher_run_cron', 'cache_refresher_run');
+    if (!wp_next_scheduled('cache_refresher_run_cron')) {
+        $offset = current_time('timestamp', false) - time();
+        $timestamp = strtotime('00:00:01') - $offset;
+        wp_schedule_event($timestamp, $cron_name, 'cache_refresher_run_cron');
+    }
+}
+
+function cache_refresher_process_post_function($post_id)
+{
+    $url = get_permalink($post_id);
+    wp_remote_get($url);
+}
+
+add_action('cache_refresher_process_post', 'cache_refresher_process_post_function', 10, 2);
+
+add_action('init', 'cache_refresher_init');
+
+function cache_refresher_register_commands() {
+    WP_CLI::add_command( 'cache-refresher refresh-all', 'cache_refresher_run' );
+}
+
+add_action('cli_init', 'cache_refresher_register_commands');
